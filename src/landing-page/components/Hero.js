c4214eb25c62b99f26d4c912840bd26f86e1fec5
Dipin <dipin.k@mastechdigital.com>
2024-04-08 10:58:42 +0700

    basic setup / outline - ready

---
+++ src/landing-page/components/Hero.js
@@ -0,0 +1,126 @@
+import * as React from 'react';
+import { alpha } from '@mui/material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import LogoCollection from './LogoCollection';
+
+export default function Hero() {
+  return (
+    <Box
+      id="hero"
+      sx={(theme) => ({
+        width: '100%',
+        backgroundImage:
+          theme.palette.mode === 'light'
+            ? 'linear-gradient(180deg, #ce93d8, #FFF)'
+            : `linear-gradient(#02294F, ${alpha('#090E10', 0.0)})`,
+        backgroundSize: '100% 20%',
+        backgroundRepeat: 'no-repeat',
+      })}
+    >
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          pt: { xs: 14, sm: 20 },
+          pb: { xs: 8, sm: 12 },
+        }}
+      >
+        <Stack spacing={2} useFlexGap sx={{ width: { xs: '100%', sm: '70%' },pb:{sm:5} }}>
+          <Typography
+            variant="h1"
+            sx={{
+              display: 'flex',
+              flexDirection: { xs: 'column', md: 'row' },
+              alignSelf: 'center',
+              textAlign: 'center',
+              fontSize: 'clamp(3.5rem, 10vw, 4rem)',
+            }}
+          >
+            Main heading&nbsp;
+            <Typography
+              component="span"
+              variant="h1"
+              sx={{
+                fontSize: 'clamp(3rem, 10vw, 4rem)',
+                color: (theme) =>
+                  theme.palette.mode === 'light' ? 'primary.main' : 'primary.light',
+              }}
+            >
+              here
+            </Typography>
+          </Typography>
+          <Typography
+            textAlign="center"
+            color="text.primary"
+            sx={{ alignSelf: 'center', width: { sm: '100%', md: '80%' } }}
+          >
+Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          </Typography>
+          {/* <Stack
+            direction={{ xs: 'column', sm: 'row' }}
+            alignSelf="center"
+            spacing={1}
+            useFlexGap
+            sx={{ pt: 2, width: { xs: '100%', sm: 'auto' } }}
+          >
+            <TextField
+              id="outlined-basic"
+              hiddenLabel
+              size="small"
+              variant="outlined"
+              aria-label="Enter your email address"
+              placeholder="Your email address"
+              inputProps={{
+                autocomplete: 'off',
+                ariaLabel: 'Enter your email address',
+              }}
+            />
+            <Button variant="contained" color="primary">
+              Start now
+            </Button>
+          </Stack> */}
+          {/* <Typography variant="caption" textAlign="center" sx={{ opacity: 0.8 }}>
+        
+            <Link href="#" color="primary">
+              Terms & Conditions
+            </Link>
+            .
+          </Typography> */}
+       
+        </Stack>
+        <LogoCollection />
+        <Box
+          id="image"
+          sx={(theme) => ({
+            mt: { xs: 8, sm: 10 },
+            alignSelf: 'center',
+            height: { xs: 200, sm: 700 },
+            width: '100%',
+            backgroundImage:
+              theme.palette.mode === 'light'
+                ? 'url("/static/images/templates/templates-images/hero-light.png")'
+                : 'url("/static/images/templates/templates-images/hero-dark.png")',
+            backgroundSize: 'cover',
+            borderRadius: '10px',
+            outline: '1px solid',
+            outlineColor:
+              theme.palette.mode === 'light'
+                ? alpha('#BFCCD9', 0.3)
+                : alpha('#9CCCFC', 0.1),
+            boxShadow:
+              theme.palette.mode === 'light'
+                ? `0 0 12px 8px ${alpha('#a4c5ff57', 0.2)}`
+                : `0 0 24px 12px ${alpha('#033363', 0.2)}`,
+          })}
+        />
+      </Container>
+    </Box>
+  );
+}
